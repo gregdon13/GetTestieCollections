65c69830b17591a7bcbbaf831985924af9c357db
Greg Donnelly <Greg@zipcodes-mbp-78.myfiosgateway.com>
2021-03-13 12:03:28 -0500

    all good

--- gettestie/src/test/java/rocks/zipcode/ComparableTests.java
+++ gettestie/src/test/java/rocks/zipcode/ComparableTests.java
@@ -1,4 +1,17 @@
 package rocks.zipcode;
 
+import org.junit.Test;
+
 public class ComparableTests {
+
+    @Test
+    public void sortTest() {
+        Integer[] intArr = new Integer[4];
+        intArr[0] = 7;
+        intArr[1] = 4;
+        intArr[2] = 2;
+        intArr[3] = 9;
+
+
+    }
 }
