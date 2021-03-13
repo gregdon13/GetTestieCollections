ce0509f29bae15915c9a40683ec9b837c1e64ce1
Greg Donnelly <Greg@zipcodes-mbp-78.myfiosgateway.com>
2021-03-12 23:18:14 -0500

    linkedlist being worked on

--- gettestie/src/test/java/rocks/zipcode/LinkedListTests.java
+++ gettestie/src/test/java/rocks/zipcode/LinkedListTests.java
@@ -1,4 +1,48 @@
 package rocks.zipcode;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.LinkedList;
+
 public class LinkedListTests {
+
+    @Test
+    public void getFirstTest() {
+        LinkedList<String> heroList = new LinkedList<>();
+        heroList.add("Hawkeye");
+        heroList.add("Vision");
+        heroList.addFirst("Cap");
+
+        String expected = "Cap";
+        String actual = heroList.getFirst();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getLastTest() {
+        LinkedList<String> heroList = new LinkedList<>();
+        heroList.add("Cap");
+        heroList.add("Hawkeye");
+        heroList.add("Vision");
+
+        String expected = "Vision";
+        String actual = heroList.getLast();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void sizeTest() {
+        LinkedList<String> heroList = new LinkedList<>();
+        heroList.add("Cap");
+        heroList.add("Hawkeye");
+        heroList.add("Vision");
+        Integer expected = 3;
+
+        Integer actual = heroList.size();
+
+        Assert.assertEquals(expected, actual);
+    }
 }
