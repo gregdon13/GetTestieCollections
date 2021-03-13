951c680b349ab334b6cc2227aec2812d619a551b
Greg Donnelly <Greg@zipcodes-mbp-78.myfiosgateway.com>
2021-03-12 22:41:26 -0500

    created LinkedListTest created. HashMap tests made

--- gettestie/src/test/java/rocks/zipcode/HashMapTest.java
+++ gettestie/src/test/java/rocks/zipcode/HashMapTest.java
@@ -1,7 +1,10 @@
 package rocks.zipcode;
 
+import org.junit.Assert;
 import org.junit.Test;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class HashMapTest {
@@ -12,5 +15,78 @@ public class HashMapTest {
         heroMap.put(1, "Cap");
         heroMap.put(2, "Iron Man");
         heroMap.put(3, "Spiderman");
+        heroMap.remove(2);
+
+        HashMap<Integer, String> expected = new HashMap<>();
+        expected.put(1, "Cap");
+        expected.put(3, "Spiderman");
+
+
+        Assert.assertEquals(expected, heroMap);
+    }
+
+    @Test
+    public void sizeTest() {
+        Integer expected = 3;
+        HashMap<Integer, String> heroMap = new HashMap<>();
+        heroMap.put(1, "Cap");
+        heroMap.put(2, "Iron Man");
+        heroMap.put(3, "Spiderman");
+
+        Integer actual = heroMap.size();
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void isEmptyAndClearTest() {
+        HashMap<Integer, String> heroMap = new HashMap<>();
+        heroMap.put(1, "Cap");
+        heroMap.put(2, "Iron Man");
+        heroMap.put(3, "Spiderman");
+
+        heroMap.clear();
+
+        Assert.assertTrue(heroMap.isEmpty());
+    }
+
+    @Test
+    public void getValueTest() {
+        HashMap<Integer, String> heroMap = new HashMap<>();
+        heroMap.put(1, "Cap");
+        heroMap.put(2, "Iron Man");
+        heroMap.put(3, "Spiderman");
+
+        String actual = heroMap.get(1);
+        String expected = "Cap";
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void containsKeyAndValueTest() {
+        HashMap<Integer, String> heroMap = new HashMap<>();
+        heroMap.put(1, "Cap");
+        heroMap.put(2, "Iron Man");
+        heroMap.put(3, "Spiderman");
+
+        Assert.assertTrue(heroMap.containsKey(3));
+        Assert.assertTrue((heroMap.containsValue("Iron Man")));
+    }
+
+    @Test
+    public void keySetTest() {
+        HashMap<Integer, String> heroMap = new HashMap<>();
+        heroMap.put(1, "Cap");
+        heroMap.put(2, "Iron Man");
+        heroMap.put(3, "Spiderman");
+
+        Integer[] actual = new Integer[3];
+        actual[0] = 1;
+        actual[1] = 2;
+        actual[2] = 3;
+
+        System.out.println(Arrays.toString(actual));
+        System.out.println(heroMap.keySet());
     }
 }
